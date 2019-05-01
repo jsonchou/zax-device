0fff6f2897543086601392ded02f2fede3c1161f
jsonchou <jsonchou@outlook.com>
2019-05-01 17:49:11 +0800

    fix: md

--- src/zax-device.js
+++ src/zax-device.js
@@ -2,7 +2,17 @@
  * device with server & client side
  */
 
+let _ua = '';
+if (typeof document !== 'undefined') {
+    _ua = navigator.userAgent.toLowerCase()
+}
+
 export default {
+    client: typeof document !== undefined,
+    server: typeof document === undefined,
+    linux: true,
+    windows: true,
+    miniprograme, //是否是小程序
     weixin: _ua.match(/micromessenger/i) == 'micromessenger',
     app: _ua.match(/zhonganwebview/i) == 'zhonganwebview',
     ipad: _ua.match(/ipad/i) == 'ipad',
@@ -14,7 +24,7 @@ export default {
     webos: _ua.match(/webos/i) == "webos",
     blackberry: _ua.match(/blackberry/i) == "blackberry",
     uc: _ua.match(/ucweb/i) == 'ucweb' || _ua.match(/ucbrowser/i) == 'ucbrowser',
-    pc: function () {
+    pc: function() {
         return !(this.ipad || this.iphone || this.android || this.wc || this.wm || this.wp || this.webos || this.blackberry || this.uc || this.weixin); //检测PC
     }
 }
