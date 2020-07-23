4383553c6fd7afb941c757d87d4e1c9f773026fa
json chou <jsonchou329@gmail.com>
2020-07-23 19:55:38 +0800

    chore(release): v3.0.2 fix: class bind bugs

--- build/scripts/release/index.js
+++ build/scripts/release/index.js
@@ -2,7 +2,7 @@
  * @Author: jsonchou
  * @Date: 2019-08-01 18:04:40
  * @Last Modified by: jsonchou
- * @Last Modified time: 2019-12-30 17:13:20
+ * @Last Modified time: 2020-07-23 19:55:06
  */
 const path = require('path');
 const chalk = require('chalk');
@@ -14,68 +14,78 @@ const checkNpm = require('./checkNpm');
 const RELEASE_LOG = process.argv[2] || '';
 
 const {
-  innerModule, needBuild, test, docs, cleanDirs,
+	innerModule, needBuild, test, docs, cleanDirs,
 } = zax;
 
 const increaseVersion = () => {
-  const prefix = version.slice(0, version.lastIndexOf('.'));
-  const suffix = version.slice(version.lastIndexOf('.') + 1);
-  return `${prefix}.${parseInt(suffix) + 1}`;
+	const prefix = version.slice(0, version.lastIndexOf('.'));
+	const suffix = version.slice(version.lastIndexOf('.') + 1);
+	return `${prefix}.${parseInt(suffix) + 1}`;
+};
+
+const decreaseVersion = () => {
+	const prefix = version.slice(0, version.lastIndexOf('.'));
+	const suffix = version.slice(version.lastIndexOf('.') + 1);
+	return `${prefix}.${parseInt(suffix) - 1}`;
 };
 
 const doRun = async (cmd) => {
-  try {
-    execSync(`npm run ${cmd}`, { stdio: 'inherit' });
-  } catch (err) {
-    console.log(`npm run ${cmd}`, err);
-    throw err;
-  }
+	try {
+		execSync(`npm run ${cmd}`, { stdio: 'inherit' });
+	} catch (err) {
+		console.log(`npm run ${cmd}`, err);
+		throw err;
+	}
 };
 
 const doPublish = async () => {
-  if (!RELEASE_LOG) {
-    console.error(chalk.bold.red('please input release log'));
-    return;
-  }
+	if (!RELEASE_LOG) {
+		console.error(chalk.bold.red('please input release log'));
+		return;
+	}
 
-  const safeNpm = checkNpm(innerModule);
+	const safeNpm = checkNpm(innerModule);
 
-  if (!safeNpm) {
-    return;
-  }
+	if (!safeNpm) {
+		return;
+	}
 
-  const version = increaseVersion();
+	cleanDirs && cleanDirs.length && await doRun('clean');
 
-  cleanDirs && cleanDirs.length && await doRun('clean');
+	test && await doRun('test');
 
-  test && await doRun('test');
+	docs && await doRun('docs');
 
-  docs && await doRun('docs');
+	needBuild && await doRun('build');
 
-  needBuild && await doRun('build');
+	let version = increaseVersion();
 
-  try {
-    execSync('git add .', { stdio: 'inherit' });
-    const logInfo = `chore(release): v${version} ${RELEASE_LOG}`;
-    execSync(`git commit -am "${logInfo}"`, { stdio: 'inherit' });
-    execSync('git push', { stdio: 'inherit' });
-  } catch (err) {
-    console.log('git', err);
-    throw err;
-  }
+	try {
+		execSync('git add .', { stdio: 'inherit' });
+		const logInfo = `chore(release): v${version} ${RELEASE_LOG}`;
+		execSync(`git commit -am "${logInfo}"`, { stdio: 'inherit' });
+		execSync('git push', { stdio: 'inherit' });
+	} catch (err) {
+		console.log('git error', err);
+		return
+	}
 
-  try {
-    execSync(`npm version ${version} `, { stdio: 'inherit' });
-    execSync('npm publish', { stdio: 'inherit' });
-  } catch (err) {
-    console.log('znpm', err);
-    throw err;
-  }
+	try {
+		// + 版本
+		execSync(`npm version ${version} `, { stdio: 'inherit' });
+		execSync('npm publish', { stdio: 'inherit' });
+	} catch (err) {
+		// - 版本
+		console.log('npm publish error', err);
+		version = decreaseVersion()
+		execSync(`npm version ${version} `, { stdio: 'inherit' });
+		return
+	}
 
-  execSync('git status', { stdio: 'inherit' });
-  execSync('git push', { stdio: 'inherit' });
+	execSync('git status', { stdio: 'inherit' });
+	execSync('git push', { stdio: 'inherit' });
 
-  doneRainbow(`version ${version} published!`);
+	doneRainbow(`version ${version} published!`);
 };
 
 doPublish();
